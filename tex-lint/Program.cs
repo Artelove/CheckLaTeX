43547dbbdf00d203d9746f2998f26739e5bd9119
Aleksandr Karagichev <66765809+Artelove@users.noreply.github.com>
2025-06-04 11:44:42 +0300

    Convert to ASP.NET controller

--- tex-lint/Program.cs
+++ tex-lint/Program.cs
@@ -1,28 +1,6 @@
-﻿using System.Diagnostics;
-using TexLint;
-using TexLint.TestFunctionClasses;
-using TexLint.Controllers;
-using TexLint.Models;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 
-class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Enter start folder name");
-        var startFile = "Thesis.tex";
-        string? startDirectory = Console.ReadLine();
-        if (string.IsNullOrWhiteSpace(startDirectory))
-        {
-            Console.WriteLine("Путь не может быть пустым");
-            return;
-        }
-        TestUtilities.StartDirectory = startDirectory;
-        var controller = new LintController(startFile, startDirectory);
-        var commands = controller.Analyze();
-        foreach (var command in commands)
-        {
-            Console.WriteLine(command);
-        }
-        Console.ReadLine();
-    }
-}
+var app = builder.Build();
+app.MapControllers();
+app.Run();
